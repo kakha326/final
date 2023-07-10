f8756ca4d6391a62534eee2845632d0b70a3d50c
kakha326 <k.mikeladze26@gmail.com>
2023-07-10 17:43:40 +0400

    burger

---
+++ script.js
@@ -0,0 +1,16 @@
+let burger = document.querySelector(".fa-bars");
+let xmark = document.querySelector(".fa-xmark");
+let mobileMenu = document.querySelector(".header_nav");
+
+
+burger.addEventListener("click", ()=>{
+    mobileMenu.classList.add("show_hide");
+    burger.style.display = "none";
+    xmark.style.display = "block";
+})
+
+xmark.addEventListener("click", ()=>{
+    mobileMenu.classList.remove("show_hide");
+    burger.style.display = "block";
+    xmark.style.display = "none";
+})
